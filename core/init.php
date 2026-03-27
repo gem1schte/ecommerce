a6ee284528351dead99db9bbc040ebc812581159
gemischte <153075050+gemischte@users.noreply.github.com>
2026-03-27 11:20:35 +0800

    chore(config): add APP_TIMEZONE and initialize timezone in bootstrap

--- core/init.php
+++ core/init.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
+date_default_timezone_set($_ENV['APP_TIMEZONE'] ?? 'UTC');
+
 if (file_exists(__DIR__ . '/../vendor/composer/platform_check.php')) {
     require __DIR__ . '/../vendor/composer/platform_check.php';
 }
